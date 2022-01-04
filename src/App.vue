3e73bf2aacda1d9b018a46df2d2f9fd06c99a9be
dengemann90 <72465696+dengemann90@users.noreply.github.com>
2022-01-04 11:01:14 +0100

    moved global styling from views to App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,24 +16,59 @@ components: {
 </script>
 
 <style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+/* Global */
+.main {
+    /*background-image: url(/Project%202/images/main_background_II.jpg);*/
+    /*background-color: #ebeded;*/
+    /*background-color: #e1a100;*/
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    /*background-image: url('./images/nutrition.jpg');*/
 }
 
-#nav {
-  padding: 30px;
-} */
+.capital {
+    min-height: 20px;
+    max-width: 1024px;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    font-weight: bold;
+    font-size: 25px;
+    background-color: #F0FFF0;
+}
 
-/* #nav a {
-  font-weight: bold;
-  color: #2c3e50;
-} */
+p.capital {
+    margin-top: 4%;
+}
 
-/* #nav a.router-link-exact-active {
-  color: #42b983;
-} */
+.appframe {
+    background-color: rgba(255, 255, 255, 0.7);
+    /*background: #5792295b;*/
+    /*border: 1px solid #7B7B7B;*/
+    box-sizing: border-box;
+    box-shadow: 0px 30px 30px rgba(0, 0, 0, 0.4);
+    border-radius: 5%;
+    max-width: 1024px;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    margin: top 25px;
+    min-height: 100vh;
+}
+
+.container_all {
+    min-height: 500px;
+    max-width: 1024px;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    background-color: red;
+}
 </style>
