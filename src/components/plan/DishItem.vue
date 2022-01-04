bffcbbef649834db759c0276b8812943dfadf712
dengemann90 <72465696+dengemann90@users.noreply.github.com>
2022-01-04 16:24:18 +0100

    change favorite status implemented

--- src/components/plan/DishItem.vue
+++ src/components/plan/DishItem.vue
@@ -4,6 +4,9 @@
         <h3>{{name}}</h3>
         <p>{{price}}</p>
         <p>{{label}}</p>
+        <div>
+            <i @click.stop="updateFavorite" class="fas regular fa-heart" :class="{active: isFavorite}"></i>
+        </div>
         <section v-if="descriptionVisible">
             <p>{{description}}</p>
         </section>
@@ -13,11 +16,17 @@
 
 <script>
 export default {
-    props: ['id', 'name', 'price', 'label', 'description'],
+    props: ['id', 'name', 'price', 'label', 'description', 'isFavorite'],
+    emits: ['change-favorite-status'],
     data(){
         return{
             descriptionVisible:false
         };
+    },
+    methods:{
+        updateFavorite(){
+            this.$emit('change-favorite-status', this.id)
+        }
     }
     
 }
@@ -47,5 +56,16 @@ button {
   display: inline-block;
   padding: 0.5rem 1.5rem;
   background-color: #383838;
+}
+
+.fas.fa-heart:hover{
+    color:red;
+}
+.fas.fa-heart{
+    color: rgba(248, 191, 191, 0.596);
+}
+
+.fas.fa-heart.active{
+    color: red;
 }
 </style>
--- src/components/plan/DishesList.vue
+++ src/components/plan/DishesList.vue
@@ -3,10 +3,13 @@
        <dish-item
        v-for="dish in dishes"
        :key="dish.id"
+       :id="dish.id"
        :name="dish.name"
        :price="dish.price"
        :label="dish.label"
        :description="dish.description"
+       :isFavorite="dish.isFavorite"
+       @change-favorite-status="changeFavoriteStatus"
        ></dish-item>
    </ul>
 </template>
@@ -25,31 +28,41 @@ export default {
                     name:'vegeatrische Pizza',
                     price: '3,5€',
                     label: 'vegetarisch',
-                    description: 'Vegetarische Pizza mit Tomaten, Brokuli und Mozzarella'
+                    description: 'Vegetarische Pizza mit Tomaten, Brokuli und Mozzarella',
+                    isFavorite:true
                 },
                                 {
                     id:2,
                     name:'Curry Tofu',
                     price: '2,5€',
                     label: 'vegetarisch',
-                    description: 'gelbes Curry mit Tofu und frischem Gemüse'
+                    description: 'gelbes Curry mit Tofu und frischem Gemüse',
+                    isFavorite:true
                 },
                                 {
                     id:3,
                     name:'vegeatrische Pizza',
                     price: '3,5€',
                     label: 'vegetarisch',
-                    description: 'Vegetarische Pizza mit Tomaten, Brokuli und Mozzarella'
+                    description: 'Vegetarische Pizza mit Tomaten, Brokuli und Mozzarella',
+                    isFavorite:false
                 },
                                 {
                     id:4,
                     name:'Curry Tofu',
                     price: '2,5€',
                     label: 'vegetarisch',
-                    description: 'gelbes Curry mit Tofu und frischem Gemüse'
+                    description: 'gelbes Curry mit Tofu und frischem Gemüse',
+                    isFavorite:false
                 }
             ]
         };
+    },
+    methods:{
+        changeFavoriteStatus(id){
+            const identifiedDish = this.dishes.find(dish => dish.id === id);
+            identifiedDish.isFavorite = !identifiedDish.isFavorite;
+        }
     }
 }
 </script>
