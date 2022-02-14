8c29cbc12f37b24b51ab9c22c47ffbe7ed2b49c4
sebastianwedell <sebastianwedell@gmx.de>
2022-02-14 12:09:45 +0100

    locals

---
+++ src/components/locations/localsList.vue
@@ -0,0 +1,16 @@
+<template>
+<!-- AUSGABE App.vue (gespeicherte Liste "locals")-->
+  <ul>
+    <locals
+      :key="id"
+      :name="name"
+      :city="city"
+      :address="address"
+      :coordinates="coordinates"
+    ></locals>
+  </ul>
+</template>
+
+<script>
+
+</script>
--- src/views/Location.vue
+++ src/views/Location.vue
@@ -5,7 +5,7 @@
         <p class="capital">Location</p>
         <div class="container_all">
           <button v-on:click="reduce(4)">test</button>
-          <!-- Hier content -->
+          <!-- Hier content AUSGABE AUS localsList.vue-->
         </div>
       </div>
     </div>
@@ -13,7 +13,10 @@
 </template>
 
 <script>
-export default {};
+//import localsList from "./locations/LocalsList.vue";
+export default {
+
+};
 </script>
 
 <style scoped>
