1f1db7455fffb2619faef2a3760d834ff75ccc99
sebastianwedell <sebastianwedell@gmx.de>
2022-02-21 19:40:18 +0100

    location dataRadius

--- src/views/Location.vue
+++ src/views/Location.vue
@@ -6,10 +6,12 @@
         <span><b>Choose radius:</b> </span>
         <input v-model="dataRadius" placeholder="" />
         <span> km</span>
+        <button v-on:click="fetchLocation"></button>
         <!-- <radius></radius> -->
         <div class="container_all">
           <!-- Hier content AUSGABE AUS localsList.vue-->
           <locals-list></locals-list>
+          <!-- <radius></radius> -->
         </div>
       </div>
     </div>
@@ -17,20 +19,50 @@
 </template>
 
 <script>
+// import { set } from "idb-keyval";
 import LocalsList from "../components/locations/LocalsList.vue";
 // import Radius from "../components/locations/Radius.vue";
 export default {
   // props: dataRadius,
+  // data() {
+  //   return {
+  //     dataRadius: this.dataRadius,
+  //   };
+  // },
   data() {
     return {
-      dataRadius: this.dataRadius,
+      geolocAggree: false,
+      dataRadius: "5",
     };
   },
   components: {
     LocalsList,
-    // radius,
+    // Radius,
   },
-  watch: {},
+  methods: {
+    async fetchLocation() {
+      // let localsList = [];
+      // let locals = [];
+
+      const responseLocal = await fetch(
+        `https://openmensa.org/api/v2/canteens?near[lat]=52.393535&near[lng]=13.127814&near[dist]=${this.dataRadius}`
+      );
+
+      const responseDataLocal = await responseLocal.json();
+      console.log(responseDataLocal);
+      // for (const key in responseDataLocal) {
+      //   const local = {
+      //     id: responseDataLocal[key].id,
+      //     name: responseDataLocal[key].name,
+      //     city: responseDataLocal[key].city,
+      //     address: responseDataLocal[key].address,
+      //     coordinates: responseDataLocal[key].coordinates,
+      //   };
+      //   locals.push(local);
+      //   console.log(locals);
+      // }
+    },
+  },
 };
 </script>
 
@@ -40,5 +72,6 @@ input {
 }
 </style>
 
-//geolocation pull
+//geolocation pull getGPSDate $ in fetch
+// modus
 // Chose your Mensa
