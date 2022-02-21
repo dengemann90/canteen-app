6fc9b3378d3aab8909f0415f8e7c2cd3f4e68982
sebastianwedell <sebastianwedell@gmx.de>
2022-02-21 16:53:03 +0100

    props location

--- src/views/Location.vue
+++ src/views/Location.vue
@@ -2,7 +2,11 @@
   <div class="main">
     <div class="appframe">
       <div class="container">
-        <p class="capital">Location</p>
+        <h1>EATERY</h1>
+        <span><b>Choose radius:</b> </span>
+        <input v-model="dataRadius" placeholder="" />
+        <span> km</span>
+        <!-- <radius></radius> -->
         <div class="container_all">
           <!-- Hier content AUSGABE AUS localsList.vue-->
           <locals-list></locals-list>
@@ -14,13 +18,27 @@
 
 <script>
 import LocalsList from "../components/locations/LocalsList.vue";
+// import Radius from "../components/locations/Radius.vue";
 export default {
+  // props: dataRadius,
+  data() {
+    return {
+      dataRadius: this.dataRadius,
+    };
+  },
   components: {
     LocalsList,
+    // radius,
   },
+  watch: {},
 };
 </script>
 
 <style scoped>
-/*  */
+input {
+  width: 4%;
+}
 </style>
+
+//geolocation pull
+// Chose your Mensa
