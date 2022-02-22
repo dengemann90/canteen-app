13e9f130d20d5dc70dda28c054f12380183a2105
sebastianwedell <sebastianwedell@gmx.de>
2022-02-22 12:00:10 +0100

    localsRadius get set

---
+++ src/components/locations/RadiusList.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- AUSGABE App.vue (gespeicherte Liste "locals")-->
+  <ul>
+    <radius-item
+      v-for="canteenRadius in localsRadius"
+      :key="canteenRadius.id"
+      :canteenRadius="canteenRadius"
+    ></radius-item>
+  </ul>
+</template>
+
+<script>
+import RadiusItem from "./RadiusItem.vue";
+import { get } from "idb-keyval";
+
+export default {
+  components: {
+    RadiusItem,
+  },
+  data() {
+    return {
+      localsRadius: [],
+    };
+  },
+  methods: {
+    getLocalsRadius() {
+      get("locals").then((data) => {
+        this.localsRadius = data;
+      });
+      console.log("LocalsRadius pulled");
+    },
+  },
+  mounted() {
+    this.getLocalsRadius();
+  },
+};
+</script>
--- src/views/Location.vue
+++ src/views/Location.vue
@@ -11,7 +11,7 @@
         <div class="container_all">
           <!-- Hier content AUSGABE AUS localsList.vue-->
           <locals-list></locals-list>
-          <!-- <radius></radius> -->
+          <radius-list></radius-list>
         </div>
       </div>
     </div>
@@ -21,7 +21,7 @@
 <script>
 // import { set } from "idb-keyval";
 import LocalsList from "../components/locations/LocalsList.vue";
-// import Radius from "../components/locations/Radius.vue";
+import { set } from "idb-keyval";
 export default {
   // props: dataRadius,
   // data() {
@@ -41,26 +41,27 @@ export default {
   },
   methods: {
     async fetchLocation() {
-      // let localsList = [];
-      // let locals = [];
+      let localsRadius = [];
 
       const responseLocal = await fetch(
         `https://openmensa.org/api/v2/canteens?near[lat]=52.393535&near[lng]=13.127814&near[dist]=${this.dataRadius}`
       );
 
       const responseDataLocal = await responseLocal.json();
+
+      for (const key in responseDataLocal) {
+        const localRadius = {
+          id: responseDataLocal[key].id,
+          name: responseDataLocal[key].name,
+          city: responseDataLocal[key].city,
+          address: responseDataLocal[key].address,
+          coordinates: responseDataLocal[key].coordinates,
+        };
+        localsRadius.push(localRadius);
+        console.log("localsRadius Json!");
+      }
       console.log(responseDataLocal);
-      // for (const key in responseDataLocal) {
-      //   const local = {
-      //     id: responseDataLocal[key].id,
-      //     name: responseDataLocal[key].name,
-      //     city: responseDataLocal[key].city,
-      //     address: responseDataLocal[key].address,
-      //     coordinates: responseDataLocal[key].coordinates,
-      //   };
-      //   locals.push(local);
-      //   console.log(locals);
-      // }
+      set("localsRadius", JSON.parse(JSON.stringify(localsRadius)));
     },
   },
 };
