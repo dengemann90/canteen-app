6edcfe46fd056a5811913a9dad01d062879c5e60
dengemann90 <72465696+dengemann90@users.noreply.github.com>
2022-02-25 16:42:09 +0100

    Filter active badge added

--- src/views/Plan.vue
+++ src/views/Plan.vue
@@ -4,6 +4,10 @@
       <div class="container">
         <p class="capital">Plan</p>
         <dish-list-date-selector @setDate="setDate"></dish-list-date-selector>
+        <div class="filter-active-badge">
+          <p class="filter-active-text">Filter aktiv </p>
+          <i class="fas solid fa-info"></i>
+        </div>
         <div class="container_all">
           <!-- Hier content -->
           <dishes-list :date-Selected="dateSelected"></dishes-list>
@@ -35,5 +39,27 @@ export default {
 </script>
 
 <style scoped>
-/*  */
+
+.filter-active-badge {
+  margin-left: 2px;
+  border-radius: 2px;
+  max-width: 50px;
+  border: solid 1px rgba(138, 169, 105, 0.3);
+  background-color: rgba(138, 169, 105, 0.25);
+  display: flex;
+}
+
+.filter-active-text {
+  margin: auto;
+  color: rgba(0, 0, 0, 0.5);
+  font-size: 8px;
+}
+
+.fas.solid.fa-info{
+ margin:auto;
+ margin-left:-1px;
+ font-size: 5px;
+ color: rgba(0, 0, 0, 0.5);
+}
+
 </style>
