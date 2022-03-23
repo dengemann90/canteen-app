b6628967c02caefb83fe3f482f37b5a4748e9a44
dengemann90 <72465696+dengemann90@users.noreply.github.com>
2022-03-23 12:14:56 +0100

    loading spinner component implemented

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store/index.js';
 import BaseDialog from './components/ui/BaseDialog.vue';
+import BaseSpinner from './components/ui/BaseSpinner.vue';
 
 const app = createApp(App);
 app.component('base-dialog', BaseDialog);
+app.component('base-spinner', BaseSpinner);
 app.use(store);
 app.use(router);
 app.mount('#app');
