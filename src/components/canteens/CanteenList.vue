46fc1d449acb0f9d6aed9dd3e2cd73cbff8efdc7
dengemann90 <72465696+dengemann90@users.noreply.github.com>
2022-03-23 13:53:54 +0100

    canteen update - fetch data (dishes)

--- src/components/canteens/CanteenList.vue
+++ src/components/canteens/CanteenList.vue
@@ -1,6 +1,10 @@
 <template>
+  <base-dialog v-if="isLoading" :open="isLoading">
+    <base-spinner></base-spinner>
+    <button @click="isLoading = false">cancel</button>
+  </base-dialog>
   <div class="canteen-list">
-    <p>Aktiv: {{ canteenName }}</p>
+    <p class="activeCanteen">Aktiv: {{ canteenName }}</p>
     <ul>
       <canteen-item
         v-for="canteen in canteenList"
@@ -16,6 +20,9 @@
 <script>
 import CanteenItem from "./CanteenItem.vue";
 import { set, get } from "idb-keyval";
+import addDays from "date-fns/addDays";
+import format from "date-fns/format";
+
 export default {
   components: {
     CanteenItem,
@@ -25,16 +32,72 @@ export default {
     return {
       selectedCanteenId: "",
       canteenName: "",
+      isLoading: false,
     };
   },
   methods: {
     updateSelectedCanteen(canteen) {
+      this.fetchData(canteen.id);
       this.selectedCanteenId = canteen.id;
       this.canteenName = canteen.name;
       this.setSelectedCanteen(canteen);
     },
     setSelectedCanteen(canteen) {
       set("selectedCanteen", JSON.parse(JSON.stringify(canteen)));
+    },
+    async fetchData(canteenId) {
+      var status = require('statuses')
+      this.isLoading = true;
+      let dateApiRequest = format(Date.now(), "yyyy-MM-dd");
+      let dateIndexedDB = Date.now();
+      let dishesPlan = [];
+
+      for (let i = 0; i <= 7; i++) {
+        let dishes = [];
+
+        const response = await fetch(
+          `https://openmensa.org/api/v2/canteens/${canteenId}/days/${dateApiRequest}/meals`
+        );
+        const responseData = await response.json().catch(error => {
+           this.isLoading= false;
+          console.log(error);
+          console.log('error: ' + response.status + ' - '+ status(response.status))
+        });
+
+        if (!response.ok) {
+          const error = new Error(
+            "failed to fetch request!"
+          );
+          throw error;
+        }
+        console.log(response)
+        console.log("fetch Gerichte openmensa api successful!");
+
+        for (const key in responseData) {
+          const dish = {
+            id: responseData[key].name,
+            category: responseData[key].category,
+            prices: responseData[key].prices,
+            notes: responseData[key].notes,
+          };
+          dishes.push(dish);
+        }
+
+        let dishesPerDay = {
+          date: format(dateIndexedDB, "d.M.yyyy"),
+          dishes: dishes,
+        };
+
+        if (dishesPerDay.dishes.length > 0) {
+          dishesPlan.push(dishesPerDay);
+        }
+
+        dateApiRequest = format(addDays(Date.now(), i + 1), "yyyy-MM-dd");
+        dateIndexedDB = addDays(dateIndexedDB, 1);
+      }
+      console.log(dishesPlan);
+      set("dishes", JSON.parse(JSON.stringify(dishesPlan)));
+       this.isLoading = false;
     },
   },
   created() {
@@ -59,7 +122,7 @@ ul {
   padding: 0;
 }
 
-p {
+.activeCanteen {
   text-align: left;
   padding-left: 1rem;
   font-family: "Roboto", sans-serif;
