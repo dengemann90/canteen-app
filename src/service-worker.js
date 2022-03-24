6a01c0d301f337aa75c39380c16740a3bfd4e65e
dengemann90 <72465696+dengemann90@users.noreply.github.com>
2022-03-24 21:17:55 +0100

    service worker set up + testing

---
+++ src/service-worker.js
@@ -0,0 +1,49 @@
+/* eslint-disable */
+
+workbox.routing.registerRoute(
+   /\.(?:css|js)$/,
+   new workbox.strategies.StaleWhileRevalidate({
+       "cacheName":"assets"
+   })
+)
+
+workbox.routing.registerRoute(
+    /\.(?:png)$/,
+    new workbox.strategies.CacheFirst({
+        "cacheName":"images"
+    })
+ )
+
+ workbox.routing.registerRoute(
+    ({ url }) => url.host.startsWith('kit.fontawesome'),
+    new workbox.strategies.CacheFirst({
+        "cacheName":"icons"
+    })
+ )
+ workbox.routing.registerRoute(
+    ({ url }) => url.host.startsWith('fonts.g'),
+    new workbox.strategies.CacheFirst({
+        "cacheName":"fonts",
+        plugins: [
+            new workbox.expiration.Plugin({
+              maxEntries: 30,
+            }),
+            new workbox.cacheableResponse.Plugin({
+              statuses: [0, 200]
+            }),
+          ],
+    })
+ )
+
+
+
+self.addEventListener('install', (event) => {
+    console.log(event);
+    console.log("Installing ....................");
+  });
+
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
+});
